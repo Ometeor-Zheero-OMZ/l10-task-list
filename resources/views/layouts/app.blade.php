d89b5b18a5bb0d04180a2687428cc9844f060eec
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-21 17:23:15 +0900

    feat: Styling the task page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>l10 Task List App</title>
-    @yield('styles')
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium text-slate-500 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+        }
+
+        .link {
+            @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+    </style>
+    {{-- blade-formatter-enable --}}
+
 </head>
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg ">
+    <h1 class="text-2xl">@yield('title')</h1>
     <div>
         {{-- if having success message, it will dissapear when you refresh the page --}}
         @if (session()->has('success'))
