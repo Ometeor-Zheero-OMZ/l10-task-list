4328f0c75b06db8e0835f6639c085d89e3e1091b
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-21 12:43:38 +0900

    feat: using form component

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,48 +2,6 @@
 
 @section('title', 'Edit Task')
 
-@section('styles')
-<style>
-    .error-message {
-        color: red;
-        font-size: 0.8rem;
-    }
-</style>
-@endsection
-
 @section('content')
-    <form method="POST" action="{{ route('tasks.store') }}">
-        @csrf
-        {{-- this directive method will add another data to be sent data property or field to be sent with --}}
-        @method('PUT')
-        <div>
-            <label for="title">
-                Title
-            </label>
-            <input type="text" name="title" id="title" value="{{ $task->title }}">
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <label for="long_description">Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <button type="submit">Edit Task</button>
-        </div>
-    </form>
+    @include('form', ['task' => $task])
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
 Route::get('/tasks', function () {
     return view('index', [
         'tasks' => Task::latest()->get()
@@ -25,28 +30,39 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
-Route::get('/tasks/{id}/edit', function ($id) {
-    return view('edit', ['task' => Task::findOrFail($id)]);
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function ($id) {
-    return view('show', ['task' => Task::findOrFail($id)]);
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', [
+        'task' => $task
+    ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
+Route::post('/tasks', function (TaskRequest $request) {
+    $task = Task::create($request->validated());
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    $task->save();
-
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully');
+})->name('tasks.destroy');
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
